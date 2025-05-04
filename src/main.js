6027c618a69cd48656b226e38d994b53449881a1
angelsantana <ignaciosantana373@gmail.com>
2025-05-04 18:10:11 +0200

    Initial commit: proyecto portfolio completo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import '../sass/main.scss'
+import '../dist/assets/style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
