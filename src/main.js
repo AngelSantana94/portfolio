2b0f5b9a95978e2ac317b1d3c94df44eb5522ba2
angelsantana <ignaciosantana373@gmail.com>
2025-12-04 20:04:18 +0100

    actualizacion final  del proyecto para la rama master

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import '../dist/assets/style.css'
+import '../sass/main.scss';
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
